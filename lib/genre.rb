2bcd299826ae35c70613bf34fdc999b1eb32c7bc
PARDEEP KAUR <pkaur84@aol.com>
2020-06-16 04:37:02 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,35 +3,3 @@ class Genre
   attr_accessor :name, :songs
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-    self.save
-  end
-
-  def self.create(name)
-    new(name)
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    self.all.clear
-  end
-
-  def add_song(song)
-    self.songs << song unless self.songs.detect { |s| s.name == song.name }
-  end
-
-  def artists
-    self.songs.collect do |song|
-      song.artist
-    end.uniq
-  end
-end
